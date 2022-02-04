4217825f0254718781297f3191310aa1da796d0d
Atanas Minkov <atanas.minkov@icloud.com>
2022-02-04 22:01:02 +0200

    MUI search box

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { TextField } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import { useContext } from "react";
@@ -7,39 +8,56 @@ import SignOutButton from "./SignOutButton";
 export function Navbar() {
   const { username, user } = useContext(UserContext);
   const photoURL = user?.photoURL ? user.photoURL : "";
-  return <nav className="navbar">
-    <ul>
-      <li>
-        <Link href="/" passHref>
-          <button className="btn-logo">self:|:made</button>
-        </Link>
-      </li>
-      {username && (
-        <>
-          <li className="push-left">
-            <SignOutButton />
-          </li>
-          <li>
-            <Link href="/admin" passHref>
-              <button className="btn-blue">Write Posts</button>
-            </Link>
-          </li>
-          <li>
-            <Link href={`/${username}`} passHref>
-              {photoURL && <Image src={photoURL} alt="user photo" width="48" height="48" />}
-            </Link>
-          </li>
-        </>
-      )}
-      {!username && (
-        <>
-          <li>
-            <Link href="/enter" passHref>
-              <button className="btn-blue">Log in</button>
-            </Link>
-          </li>
-        </>
-      )}
-    </ul>
-  </nav>
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/" passHref>
+            <button className="btn-logo">self:|:made</button>
+          </Link>
+        </li>
+        <li style={{ width: "auto" }}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Search"
+            variant="standard"
+          />
+        </li>
+        {username && (
+          <>
+            <li className="push-left">
+              <SignOutButton />
+            </li>
+            <li>
+              <Link href="/admin" passHref>
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`} passHref>
+                {photoURL && (
+                  <Image
+                    src={photoURL}
+                    alt="user photo"
+                    width="48"
+                    height="48"
+                  />
+                )}
+              </Link>
+            </li>
+          </>
+        )}
+        {!username && (
+          <>
+            <li>
+              <Link href="/enter" passHref>
+                <button className="btn-blue">Log in</button>
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
